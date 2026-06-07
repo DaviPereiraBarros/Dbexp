645d726e23ff78f15e56431d0f188f06ad9b26a7
Davi Pereira Barros <103952649+DaviPereiraBarros@users.noreply.github.com>
2026-06-07 19:34:39 -0300

    Add favicon icon to metadata in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dbexp",
   description: "",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
